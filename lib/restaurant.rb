630023fe57afcbc089a46811ec4028c9605565b9
khaledsf <kbouchama@gmail.com>
2016-07-22 16:44:47 -0700

    Added *save* method

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -48,13 +48,13 @@ class Restaurant
 		@price   = args[:price]   || ""	
 	end
 
-	# def save
-	# 	return false unless Restaurant.file_usable?
-	# 	File.open(@@filepath, 'a') do |file|
-	# 		file.puts "#{[@name, @cuisine, @price].join("\t")}\n"
-	# 	end
-	# 	return true
-	# end
+	def save
+		return false unless Restaurant.file_usable?
+		File.open(@@filepath, 'a') do |file|
+			file.puts "#{[@name, @cuisine, @price].join("\t")}\n"
+		end
+		return true
+	end
 
 end
 
